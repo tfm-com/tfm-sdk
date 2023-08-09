1b1733c66a8d9cf41ea16086c678f09848b7405e
0xTFM <140174482+0xTFM@users.noreply.github.com>
2023-08-09 09:45:22 +0300

    fix(sdk.ts): fix common route types

--- packages/sdk.ts/src/v1/ibc/dto/route/route.dto.ts
+++ packages/sdk.ts/src/v1/ibc/dto/route/route.dto.ts
@@ -1,4 +1,9 @@
-export interface IbcRouteOperationDto {
+export enum RouteType {
+  ibc_transfer = "ibc_transfer",
+  chain_swap_operations = "chain_swap_operations",
+}
+
+export interface Operation {
   offerToken: string;
   offerChain: string;
   askToken: string;
@@ -8,24 +13,54 @@ export interface IbcRouteOperationDto {
   contractAddr: string;
 }
 
-export interface RouteComponentDto {
-  inputAmount: number;
-  returnAmount: number;
-  priceImpact: number;
-  inputPercent: number;
-  operations: IbcRouteOperationDto[];
+export interface ChainRoute {
+  inputAmount: string;
+  returnAmount: string;
+  priceImpact: string;
+  inputPercent: string;
+  operations: Operation[];
+}
+
+export interface ChainSwapOperationsRoute {
+  type: RouteType.chain_swap_operations;
+  inputAmount: string;
+  returnAmount: string;
+  priceImpact: string;
+  chainName: string;
+  chainId: string;
+  routes: ChainRoute[];
+  alternatives: Record<string, ChainRoute>;
+  wasmHookTransaction: boolean;
+  contractIbcForward: boolean;
+}
+
+export interface IBCTransferRoute {
+  type: RouteType.ibc_transfer;
+  inputAmount: string;
+  returnAmount: string;
+  sourceDenom: string;
+  destinationDenom: string;
+  sourceChainId: string;
+  destinationChainId: string;
+  sourceChainName: string;
+  destinationChainName: string;
+  sourceChannel: string;
+  destinationChannel: string;
+  sourcePort: string;
+  destinationPort: string;
+  pfmTransaction: boolean;
 }
 
 export interface CommonRouteDto {
-  inputAmount: number;
-  returnAmount: number;
+  inputAmount: string;
+  returnAmount: string;
   sourceChainId: string;
   destinationChainId: string;
   sourceChainName: string;
   destinationChainName: string;
   askToken: string;
   offerToken: string;
-  routes: RouteComponentDto[];
+  routes: (IBCTransferRoute | ChainSwapOperationsRoute)[];
 }
 
 export type TransferRouteDto = CommonRouteDto;
